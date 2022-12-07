c1780495f1a05c8cd817a492862191ae2298d589
gabrielkurzydlowski <107561461+gabrielkurzydlowski@users.noreply.github.com>
2022-12-07 10:06:49 -0300

    Add files via upload

---
+++ js_ex11.js
@@ -0,0 +1,11 @@
+function soma(){
+    var n1,n2,res
+    n1=document,getElementById
+    ("n1").value)
+    n2=document.getElementById
+    ("n2").value)
+    res=n1+n2
+    document.getElementById("Resultado")
+    innerHTML='o resultado da soma e $ '{res}
+    
+}
